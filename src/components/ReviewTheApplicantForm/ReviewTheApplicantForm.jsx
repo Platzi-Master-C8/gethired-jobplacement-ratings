ce95c266ff4e15a54c92cc4f9a164611e06831d4
Andres <andres-lh@outlook.com>
2021-12-03 19:30:59 -0500

    feat: creation of the applicant's review form

---
+++ src/components/ReviewTheApplicantForm/ReviewTheApplicantForm.jsx
@@ -0,0 +1,261 @@
+import React, { useState } from 'react';
+
+import {
+    FormControl,
+    OutlinedInput,
+    Grid,
+    Typography,
+    FormControlLabel,
+    Box,
+    Button,
+    Modal,
+    RadioGroup,
+    Radio,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
+
+const CustomBox = styled(Box)({
+    '.MuiSvgIcon-root': {
+        fontSize: 30,
+        cursor: 'pointer',
+        marginRight: '10%',
+    },
+});
+
+const boxStyles = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '45%',
+    height: '75%',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+    overflow: 'hidden',
+};
+
+const ReviewTheApplicantForm = () => {
+    const [open, setOpen] = useState(false);
+
+    const [review, setReview] = useState({
+        name: '',
+        hired: null,
+        comunication: undefined,
+        confidence: undefined,
+        negotiation: undefined,
+        motivation: undefined,
+        selfKnowledge: undefined,
+        hardSkills: undefined,
+    });
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setReview({ ...review, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(review);
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Grid md={12} sx={{ display: 'grid', justifyContent: 'flex-end' }}>
+                <Button onClick={handleOpen}>Review the applicant </Button>
+            </Grid>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <form onSubmit={handleSubmit}>
+                    <Box sx={boxStyles}>
+                        <Typography variant="h1" align="center" sx={{ fontSize: 28 }}>
+                            Review the Applicant
+                        </Typography>
+                        <Grid container marginTop={5}>
+                            <Grid item md={6}>
+                                <Typography variant="subtitle1" marginBottom="20px" sx={{ fontSize: 18 }}>
+                                    Applicant name
+                                </Typography>
+                                <FormControl sx={{ width: '85%' }} required>
+                                    <OutlinedInput
+                                        id="name"
+                                        name="name"
+                                        placeholder="Aplicant name"
+                                        value={review.name}
+                                        onChange={handleInput}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item md={6}>
+                                <Typography variant="subtitle1" marginBottom="30px" sx={{ fontSize: 18 }}>
+                                    Was he/she hired?
+                                </Typography>
+                                <FormControl component="fieldset" required>
+                                    <RadioGroup id="hired" name="hired" row value={review.hired} onChange={handleInput}>
+                                        <FormControlLabel value control={<Radio />} label="Yes" />
+                                        <FormControlLabel value={false} control={<Radio />} label="No" />
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                        <Grid marginTop={3}>
+                            <Typography variant="h2">Skill Assessment</Typography>
+                            <Grid container rowSpacing={1} alignItems="center" marginTop="5px">
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Communication</Typography>
+                                </Grid>
+                                <Grid item md={6} sm={12}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.comunication ? (
+                                                    <StarIcon
+                                                        onClick={() =>
+                                                            setReview({ ...review, comunication: index + 1 })
+                                                        }
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() =>
+                                                            setReview({ ...review, comunication: index + 1 })
+                                                        }
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Confidence</Typography>
+                                </Grid>
+                                <Grid item md={6} sm={12}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.confidence ? (
+                                                    <StarIcon
+                                                        onClick={() => setReview({ ...review, confidence: index + 1 })}
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() => setReview({ ...review, confidence: index + 1 })}
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Negotiation</Typography>
+                                </Grid>
+                                <Grid item md={6}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.negotiation ? (
+                                                    <StarIcon
+                                                        onClick={() => setReview({ ...review, negotiation: index + 1 })}
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() => setReview({ ...review, negotiation: index + 1 })}
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Motivation</Typography>
+                                </Grid>
+                                <Grid item md={6} sm={12}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.motivation ? (
+                                                    <StarIcon
+                                                        onClick={() => setReview({ ...review, motivation: index + 1 })}
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() => setReview({ ...review, motivation: index + 1 })}
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Self-knowledge</Typography>
+                                </Grid>
+                                <Grid item md={6} sm={12}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.selfKnowledge ? (
+                                                    <StarIcon
+                                                        onClick={() =>
+                                                            setReview({ ...review, selfKnowledge: index + 1 })
+                                                        }
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() =>
+                                                            setReview({ ...review, selfKnowledge: index + 1 })
+                                                        }
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                                <Grid item md={5} sm={12}>
+                                    <Typography variant="subtitle2">Hard Skills</Typography>
+                                </Grid>
+                                <Grid item md={6} sm={12}>
+                                    <CustomBox>
+                                        {[...Array(5)].map((star, index) => (
+                                            <div>
+                                                {index < review.hardSkills ? (
+                                                    <StarIcon
+                                                        onClick={() => setReview({ ...review, hardSkills: index + 1 })}
+                                                    />
+                                                ) : (
+                                                    <StarBorderIcon
+                                                        onClick={() => setReview({ ...review, hardSkills: index + 1 })}
+                                                    />
+                                                )}
+                                            </div>
+                                        ))}
+                                    </CustomBox>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid container sx={{ alignItems: 'center', justifyContent: 'center', marginTop: 5 }}>
+                            <Grid item md={2} onClick={handleClose}>
+                                <Button variant="outlined">Cancel</Button>
+                            </Grid>
+                            <Grid item md={2}>
+                                <Button variant="outlined" type="submit">
+                                    Submit
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </form>
+            </Modal>
+        </div>
+    );
+};
+
+export default ReviewTheApplicantForm;
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { GeneralCompanyRate } from '../components/GeneralCompanyRate';
 import CompanyTabs from '../components/CompanyTabs';
 
+//
+import ReviewTheApplicantForm from '../components/ReviewTheApplicantForm/ReviewTheApplicantForm';
+
 const Company = () => {
     return (
         <main>
             <GeneralCompanyRate />
             <CompanyTabs />
+            <ReviewTheApplicantForm />
         </main>
     );
 };
