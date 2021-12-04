22697d2ab156ee68b60b86469a7d1e1e9a90313f
fersdeveloper <fersdeveloper@gmail.com>
2021-12-04 10:19:41 -0600

    feat: add the Reviews component to the page of Company

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { GeneralCompanyRate } from '../components/GeneralCompanyRate';
 import CompanyTabs from '../components/CompanyTabs';
+import { Reviews } from '../components/Reviews';
 
 const Company = () => {
     return (
         <main>
             <GeneralCompanyRate />
             <CompanyTabs />
+            <Reviews />
         </main>
     );
 };
