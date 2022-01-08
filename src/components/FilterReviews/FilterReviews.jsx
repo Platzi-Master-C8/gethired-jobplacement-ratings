53ad924f80ce650d1f166963b47f126a72a4ed4f
Manuel Mendoza <manuel_mm97@hotmail.com>
2022-01-08 16:17:15 -0600

    feat: add filters to reviews

---
+++ src/components/FilterReviews/FilterReviews.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { Box, Button, InputLabel, FormControl, MenuItem, TextField, Typography, Select } from '@mui/material';
+
+const FilterReviewsWrapper = styled.div`
+    background-color: #ddd;
+    border-radious: 1rem;
+    margin-bottom: 1rem;
+    padding: 1rem;
+`;
+
+const TextFieldS = styled(TextField)`
+    background-color: #fff;
+    border-radius: 4px;
+    margin: 0 1rem;
+    width: 50%;
+    @media (max-width: 768px) {
+        width: 40%;
+    }
+    @media (max-width: 480px) {
+        width: 100%;
+        margin: 1rem 0;
+    }
+`;
+
+const FormControlS = styled(FormControl)`
+    background-color: #fff;
+    border-radius: 4px;
+    width: 35%;
+    @media (max-width: 768px) {
+        width: 30%;
+    }
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+`;
+
+const RowBox = styled(Box)`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    @media (max-width: 480px) {
+        flex-direction: column;
+    }
+`;
+
+const SortBox = styled(RowBox)`
+    @media (max-width: 480px) {
+        flex-direction: row;
+        margin-top: 1rem;
+    }
+`;
+
+const FilterReviews = ({ handleSearch, sortCriteria, toggleSortCriteria }) => {
+    const [filterValue, setFilterValue] = useState('job_title');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') handleSearch(searchQuery, filterValue);
+    };
+
+    return (
+        <FilterReviewsWrapper>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    borderBottom: '2px solid #FFF',
+                }}
+            >
+                <Typography variant="subtitle1" sx={{ mb: 2 }}>
+                    Search Reviews
+                </Typography>
+                <RowBox sx={{ mb: 2 }}>
+                    <FormControlS>
+                        <InputLabel id="filter-select-label">Search criteria</InputLabel>
+                        <Select
+                            labelId="filter-select-label"
+                            id="filter-select"
+                            value={filterValue}
+                            label="Age"
+                            onChange={(e) => setFilterValue(e.target.value)}
+                        >
+                            <MenuItem value="job_title">Job title</MenuItem>
+                            <MenuItem value="content_type">Review content</MenuItem>
+                            <MenuItem value="job_location">Location</MenuItem>
+                        </Select>
+                    </FormControlS>
+                    <TextFieldS
+                        id="search-input"
+                        label="Search Reviews"
+                        variant="outlined"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onKeyPress={handleEnter}
+                    />
+                    <Button variant="contained" onClick={() => handleSearch(searchQuery, filterValue)}>
+                        Search
+                    </Button>
+                </RowBox>
+            </Box>
+            <RowBox sx={{ mt: 2 }}>
+                <Typography variant="subtitle1" sx={{ mr: 2 }}>
+                    Sort by:
+                </Typography>
+                <SortBox>
+                    <Button
+                        variant={sortCriteria.sortKey === 'utility_counter' ? 'contained' : 'outlined'}
+                        endIcon={
+                            sortCriteria.sortKey === 'utility_counter' ? (
+                                sortCriteria.orientation === 'desc' ? (
+                                    <ArrowUpwardIcon />
+                                ) : (
+                                    <ArrowDownwardIcon />
+                                )
+                            ) : null
+                        }
+                        onClick={() => toggleSortCriteria('utility_counter')}
+                    >
+                        Helpfulness
+                    </Button>
+                    <Button
+                        variant={sortCriteria.sortKey === 'weighted_average_per_evaluation' ? 'contained' : 'outlined'}
+                        endIcon={
+                            sortCriteria.sortKey === 'weighted_average_per_evaluation' ? (
+                                sortCriteria.orientation === 'desc' ? (
+                                    <ArrowUpwardIcon />
+                                ) : (
+                                    <ArrowDownwardIcon />
+                                )
+                            ) : null
+                        }
+                        onClick={() => toggleSortCriteria('weighted_average_per_evaluation')}
+                    >
+                        Rating
+                    </Button>
+                    <Button
+                        variant={sortCriteria.sortKey === 'created_at' ? 'contained' : 'outlined'}
+                        endIcon={
+                            sortCriteria.sortKey === 'created_at' ? (
+                                sortCriteria.orientation === 'desc' ? (
+                                    <ArrowUpwardIcon />
+                                ) : (
+                                    <ArrowDownwardIcon />
+                                )
+                            ) : null
+                        }
+                        onClick={() => toggleSortCriteria('created_at')}
+                    >
+                        Date
+                    </Button>
+                </SortBox>
+            </RowBox>
+        </FilterReviewsWrapper>
+    );
+};
+
+FilterReviews.propTypes = {
+    handleSearch: PropTypes.func.isRequired,
+    sortCriteria: PropTypes.shape({
+        sortKey: PropTypes.string.isRequired,
+        orientation: PropTypes.string.isRequired,
+    }).isRequired,
+    toggleSortCriteria: PropTypes.func.isRequired,
+};
+
+export default FilterReviews;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import { ReviewCard } from '../ReviewCard';
+import { FilterReviews } from '../FilterReviews';
 
 import api from '../../services/api';
 
@@ -8,6 +9,8 @@ const Reviews = () => {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [list, setList] = useState([]);
+    const [data, setData] = useState([]);
+    const [sortCriteria, setSortCriteria] = useState({ sortKey: 'created_at', orientation: 'asc' });
 
     useEffect(() => {
         api.companyEvaluations
@@ -16,12 +19,27 @@ const Reviews = () => {
             .then((result) => {
                 setIsLoaded(true);
                 setList(result);
+                setData(result);
             })
             .catch((e) => {
                 setIsLoaded(true);
                 setError(e);
             });
     }, []);
+
+    const handleSearch = (query, attribute) => {
+        setList(data.filter((x) => x[attribute].toLowerCase().includes(query.toLowerCase().trim())));
+    };
+
+    const toggleSortCriteria = (sortKey) => {
+        const orientation =
+            sortCriteria.sortKey === sortKey ? (sortCriteria.orientation === 'asc' ? 'desc' : 'asc') : 'asc';
+        const sortedList = list.sort((a, b) =>
+            a[sortKey] > b[sortKey] ? (orientation === 'asc' ? 1 : -1) : orientation === 'asc' ? -1 : 1,
+        );
+        setList(sortedList);
+        setSortCriteria({ sortKey, orientation });
+    };
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -31,18 +49,25 @@ const Reviews = () => {
     }
 
     return (
-        <Box
-            sx={{
-                display: 'grid',
-                gap: 2,
-                gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 25rem), 1fr))',
-                p: 1,
-            }}
-        >
-            {list.map((review) => (
-                <ReviewCard key={review.id} review={review} />
-            ))}
-        </Box>
+        <Fragment>
+            <FilterReviews
+                handleSearch={handleSearch}
+                sortCriteria={sortCriteria}
+                toggleSortCriteria={toggleSortCriteria}
+            />
+            <Box
+                sx={{
+                    display: 'grid',
+                    gap: 2,
+                    gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 25rem), 1fr))',
+                    p: 1,
+                }}
+            >
+                {list.map((review) => (
+                    <ReviewCard key={review.id} review={review} />
+                ))}
+            </Box>
+        </Fragment>
     );
 };
 
