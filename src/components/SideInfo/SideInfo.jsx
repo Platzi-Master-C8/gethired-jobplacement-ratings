70ee5599b13d6f1027e8192f75dd32a5c8073d48
Andres <andres-lh@outlook.com>
2022-01-16 20:57:25 -0500

    feat: overview added to tabs and side info changed from company to reviews

--- src/components/SideInfo/SideInfo.jsx
+++ src/components/SideInfo/SideInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -8,21 +8,17 @@ import CompanyTabs from '../components/CompanyTabs';
 import { ReviewTheApplicantForm } from '../components/ReviewTheApplicantForm';
 import { ReviewApplicationProcessForm } from '../components/ReviewApplicationProcess';
 
-import { SideInfo } from '../components/SideInfo';
-import { useMediaQuery } from '../hooks';
-
 // Tabs
 import { Reviews } from '../components/Reviews';
+import { Overview } from '../components/Overview';
 
 const tabsOptions = [
-    { tabKey: 'Overview', tabContent: 'Overview' },
+    { tabKey: 'Overview', tabContent: <Overview /> },
     { tabKey: 'Reviews', tabContent: <Reviews /> },
     { tabKey: 'Jobs', tabContent: 'Jobs' },
 ];
 
 const Company = () => {
-    const isMobile = useMediaQuery('(max-width: 480px)');
-
     return (
         <Container>
             <Header isLogged />
