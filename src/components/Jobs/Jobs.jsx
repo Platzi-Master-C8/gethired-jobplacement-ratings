c968bcb33d44c5e16094f859adc7d8f9d84f5705
Manuel Mendoza <manuel_mm97@hotmail.com>
2022-02-03 22:47:23 -0600

    feat: applicant forms conections to Backend

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -4,8 +4,8 @@ import { ReviewApplicationProcessForm } from '../ReviewApplicationProcess';
 
 const Jobs = () => (
     <div>
-        <ReviewTheApplicantForm />
-        <ReviewApplicationProcessForm />
+        <ReviewTheApplicantForm company_id={1} applicant_id={0} />
+        <ReviewApplicationProcessForm company_id={1} applicant_id={0} />
     </div>
 );
 
